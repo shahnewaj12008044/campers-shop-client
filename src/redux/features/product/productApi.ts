ed74b68482f19fbd8c61314d10d9b35b9b587b42
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-09-22 14:39:57 +0600

    filter by category done

--- src/redux/features/product/productApi.ts
+++ src/redux/features/product/productApi.ts
@@ -1,19 +1,23 @@
+import Category from "@/pages/Home/Category/Category";
 import { baseApi } from "@/redux/api/baseApi";
 
 const productApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getAllProduct: builder.query({
-      query: ({ search, price }) => {
+      query: ({ search, price, category }) => {
         let queryString = `/products?`;
         if (search) queryString += `searchTerm=${search}&`;
         if(price){
           const priceRange = price.split("-")
-          console.log(priceRange)
+          // console.log(priceRange)
           queryString +=`minPrice=${priceRange[0]}&maxPrice=${priceRange[1]}`
-          console.log("inside",queryString)
+          // console.log("inside",queryString)
+        }
+        if(category){
+          queryString+=`category=${category}`
         }
 
-        // console.log(queryString)
+        console.log(queryString)
 
         return {
           method: "GET",
