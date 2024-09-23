ac44e3bdc4e35eb83b310018bbfb8ae7a1859ef6
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-09-23 10:43:51 +0600

    redirects add for netlify

--- src/pages/PaymentSuccess/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess/PaymentSuccess.tsx
@@ -1,5 +1,6 @@
 
 import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const PaymentSuccess = () => {
   return (
@@ -8,9 +9,9 @@ const PaymentSuccess = () => {
         <AiOutlineCheckCircle className="text-green-500 w-16 h-16 mx-auto mb-4" />
         <h1 className="text-2xl font-bold text-gray-800 mb-2">Payment Successful!</h1>
         <p className="text-gray-600 mb-4">Thank you for your purchase. Your payment has been processed successfully.</p>
-        <button className="bg-[#020C29] text-white px-4 py-2 rounded-md hover:bg-lime-400 transition duration-300">
-          Go to Dashboard
-        </button>
+      <Link to="/">  <button className="bg-[#020C29] text-white px-4 py-2 rounded-md hover:bg-lime-400 transition duration-300">
+          Continue Purchase
+        </button></Link>
       </div>
     </div>
   );
